6c8e07756e06a79cc28c147dd3fb4a338d2ba3ea
naufalziyad <naufal.polinema@gmail.com>
2024-08-30 16:34:17 +0700

    add: task Form

---
+++ src/TaskForm.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export default function TaskForm({onAdd}) {
+    const[taskName, setTaskName] = useState('');
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        console.log(taskName);
+        onAdd(taskName);
+        setTaskName('');
+    }
+    
+    return(
+    <form onSubmit={handleSubmit}>
+      <button>+</button>
+      <input type="text"
+        value={taskName} 
+        onChange={ev => setTaskName(ev.target.value)} 
+        placeholder="Your next task" />  
+    </form>
+    )
+} 

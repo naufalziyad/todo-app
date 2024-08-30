bfede4d5dd4a99abb8bfaeb9f1fd6b327dd09269
naufalziyad <naufal.polinema@gmail.com>
2024-08-30 16:34:42 +0700

    add: functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import TaskForm from "./TaskForm";
+import Task from "./Task";
+import {useEffect, useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks || []);
+  }, []);
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name:name,done:false}];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(prev => {
+      return prev.filter((taskObject,index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter(t => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = numberComplete/numberTotal * 100;
+    if (percentage === 0) {
+      return 'Isi minimal 1 saja! 🙏';
+    }
+    if (percentage === 100) {
+      return 'Mantap waktunya bersantai! 🏝';
+    }
+    return 'Yuk semangat! 💪🏻';
+  }
+
+  function renameTask(index,newName) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>{numberComplete}/{numberTotal} Selesai</h1>
+      <h2>{getMessage()}</h2>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task,index) => (
+        <Task key={index} {...task}
+              onRename={newName => renameTask(index,newName)}
+              onTrash={() => removeTask(index)}
+              onToggle={done => updateTaskDone(index, done)} />
+      ))}
+    </main>
+  );
 }
 
-export default App
+export default App;
